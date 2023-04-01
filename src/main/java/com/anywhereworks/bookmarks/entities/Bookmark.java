ff010fde569be4a98a0c25f0d7109ad9cade8981
Amrit Kaur <amritkaur2301@gmail.com>
2023-04-01 22:06:29 +0530

    feat: add bidirectional mapping from Bookmark to Folder

--- src/main/java/com/anywhereworks/bookmarks/entities/Bookmark.java
+++ src/main/java/com/anywhereworks/bookmarks/entities/Bookmark.java
@@ -1,59 +1,83 @@
-package com.example.TopicProject.entities;
+package com.anywhereworks.bookmarks.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 
 @Entity
-@Table(name = "topics")
-public class Topic {
+@Table(name = "bookmarks")
+public class Bookmark {
 
 	@Id
-	//@GeneratedValue(strategy = GenerationType.AUTO)
-	private String id;
-	
-	@Column(nullable = false, name = "topicName")
-	private String name;
-	private String description;
-	
-	public Topic() {
-		
+	@GeneratedValue(strategy = GenerationType.SEQUENCE)
+	private int bookmarkId;
+
+	@Column(nullable = false)
+	private String title;
+	private String url;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "FolderId")
+	@JsonIgnore
+	private Folder folder;
+
+	public Bookmark() {
+
 	}
-	
-	public Topic(String id, String name, String description) {
+
+	public Bookmark(int bookmarkId, String title, String url) {
 		super();
-		this.id = id;
-		this.name = name;
-		this.description = description;
+		this.bookmarkId = bookmarkId;
+		this.title = title;
+		this.url = url;
+
 	}
 
-	public String getId() {
-		return id;
+	public int getBookmarkId() {
+		return bookmarkId;
 	}
 
-	public void setId(String id) {
-		this.id = id;
+	public void setBookmarkId(int bookmarkId) {
+		this.bookmarkId = bookmarkId;
 	}
 
-	public String getName() {
-		return name;
+	public String getTitle() {
+		return title;
 	}
 
-	public void setName(String name) {
-		this.name = name;
+	public void setTitle(String title) {
+		this.title = title;
 	}
 
-	public String getDescription() {
-		return description;
+	public String getUrl() {
+		return url;
 	}
 
-	public void setDescription(String description) {
-		this.description = description;
+	public void setUrl(String url) {
+		this.url = url;
 	}
 
-	
+	public Folder getFolder() {
+		return folder;
+	}
+
+	public void setFolder(Folder folder) {
+		this.folder = folder;
+	}
+
+	public int getFolderId() {
+		if(folder!=null)
+			return folder.getId();
+		return 0;
+	}
 
 }
