d9a547ec9f2b227a79e6961f237d3c96a19b2152
Amrit Kaur <amritkaur2301@gmail.com>
2023-04-01 22:12:32 +0530

    refactor

--- src/main/java/com/anywhereworks/bookmarks/repositories/BookmarkRepository.java
+++ src/main/java/com/anywhereworks/bookmarks/repositories/BookmarkRepository.java
@@ -1,18 +1,12 @@
-package com.example.TopicProject.repositories;
+package com.anywhereworks.bookmarks.repositories;
 
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
-import com.example.TopicProject.entities.Topic;
+import com.anywhereworks.bookmarks.entities.Bookmark;
 
-public interface TopicRepository extends CrudRepository<Topic, String> {
+public interface BookmarkRepository extends CrudRepository<Bookmark, String> {
 
-	/*
-	 * the framework sees the embedded Derby database in the classpath and assumes
-	 * that to be the database to connect to. No connection info necessary.
-	 */
-	@Query("select t from Topic t where t.name = ?1")
-	Optional<Topic> findByName(String name);
 }
