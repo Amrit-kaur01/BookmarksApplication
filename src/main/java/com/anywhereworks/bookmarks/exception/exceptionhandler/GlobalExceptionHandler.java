bb7e95e77798fa53a645ea2ff34d66908ba175a4
Amrit Kaur <amritkaur2301@gmail.com>
2023-04-23 12:45:18 +0530

    feat: add ExceptionHandler for OptimisticLockException

--- src/main/java/com/anywhereworks/bookmarks/exception/exceptionhandler/GlobalExceptionHandler.java
+++ src/main/java/com/anywhereworks/bookmarks/exception/exceptionhandler/GlobalExceptionHandler.java
@@ -1,5 +1,9 @@
 package com.anywhereworks.bookmarks.exception.exceptionhandler;
 
+import java.sql.SQLException;
+import java.time.LocalDateTime;
+
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -7,6 +11,8 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExcep
 
 import com.anywhereworks.bookmarks.exception.custom.BusinessException;
 import com.anywhereworks.bookmarks.exception.custom.ErrorResponse;
+
+import jakarta.persistence.OptimisticLockException;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
@@ -17,4 +23,19 @@ public class GlobalExceptionHandler {
 				businessException.getMessage(), businessException.getTimestamp());
 		return new ResponseEntity(errorResponse, businessException.getHttpStatus());
 	}
+	
+	@ExceptionHandler(value = SQLException.class)
+	public ResponseEntity<Object> handleSQLException(SQLException sqlException) {
+		ErrorResponse errorResponse = new ErrorResponse(HttpStatus.CONFLICT,
+				sqlException.getMessage(), LocalDateTime.now());
+		return new ResponseEntity(errorResponse, HttpStatus.CONFLICT);
+	}
+	
+	@ExceptionHandler(value = OptimisticLockException.class)
+	public ResponseEntity<Object> handleOptimisticLockException(BusinessException optimisticLockException) {
+		ErrorResponse errorResponse = new ErrorResponse(HttpStatus.CONFLICT,
+				optimisticLockException.getMessage(), LocalDateTime.now());
+		return new ResponseEntity(errorResponse,HttpStatus.CONFLICT);
+	}
+	
 }
