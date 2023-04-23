e597e7d705e9050af1abaf671878b766ed3e7468
Amrit Kaur <amritkaur2301@gmail.com>
2023-04-23 12:11:38 +0530

    feat: add annotation for enabling transaction and caching

--- src/main/java/com/anywhereworks/bookmarks/BookmarksApplication.java
+++ src/main/java/com/anywhereworks/bookmarks/BookmarksApplication.java
@@ -2,8 +2,13 @@ package com.anywhereworks.bookmarks;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
+@EnableTransactionManagement
+@EnableCaching
 public class BookmarksApplication {
 
 	public static void main(String[] args) {
