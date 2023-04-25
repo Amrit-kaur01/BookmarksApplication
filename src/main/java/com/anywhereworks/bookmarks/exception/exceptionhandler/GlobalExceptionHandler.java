d9423840c12bfab99f416c66db4b9505dda530cc
Amrit Kaur <amritkaur2301@gmail.com>
2023-04-25 12:25:19 +0530

    refactor: parameterize generic type ResponseEntity<T>

--- src/main/java/com/anywhereworks/bookmarks/exception/exceptionhandler/GlobalExceptionHandler.java
+++ src/main/java/com/anywhereworks/bookmarks/exception/exceptionhandler/GlobalExceptionHandler.java
@@ -1,13 +1,11 @@
 package com.anywhereworks.bookmarks.exception.exceptionhandler;
 
-import java.sql.SQLException;
 import java.time.LocalDateTime;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import com.anywhereworks.bookmarks.exception.custom.BusinessException;
 import com.anywhereworks.bookmarks.exception.custom.ErrorResponse;
@@ -18,17 +16,17 @@ import jakarta.persistence.OptimisticLockException;
 public class GlobalExceptionHandler {
 
 	@ExceptionHandler(value = BusinessException.class)
-	public ResponseEntity<Object> handleBusinessException(BusinessException businessException) {
+	public ResponseEntity<ErrorResponse> handleBusinessException(BusinessException businessException) {
 		ErrorResponse errorResponse = new ErrorResponse(businessException.getHttpStatus(),
 				businessException.getMessage(), businessException.getTimestamp());
-		return new ResponseEntity(errorResponse, businessException.getHttpStatus());
+		return new ResponseEntity<>(errorResponse, businessException.getHttpStatus());
 	}
-	
+
 	@ExceptionHandler(value = OptimisticLockException.class)
-	public ResponseEntity<Object> handleOptimisticLockException(BusinessException optimisticLockException) {
-		ErrorResponse errorResponse = new ErrorResponse(HttpStatus.CONFLICT,
-				optimisticLockException.getMessage(), LocalDateTime.now());
-		return new ResponseEntity(errorResponse,HttpStatus.CONFLICT);
+	public ResponseEntity<ErrorResponse> handleOptimisticLockException(BusinessException optimisticLockException) {
+		ErrorResponse errorResponse = new ErrorResponse(HttpStatus.CONFLICT, optimisticLockException.getMessage(),
+				LocalDateTime.now());
+		return new ResponseEntity<>(errorResponse, HttpStatus.CONFLICT);
 	}
-	
+
 }
