2dff17fafad2a62b9903e9a07c23868ae0fd4cac
Amrit Kaur <amritkaur2301@gmail.com>
2023-04-25 12:29:24 +0530

    style: code format

--- src/main/java/com/anywhereworks/bookmarks/repositories/BookmarkRepository.java
+++ src/main/java/com/anywhereworks/bookmarks/repositories/BookmarkRepository.java
@@ -1,8 +1,5 @@
 package com.anywhereworks.bookmarks.repositories;
 
-import java.util.Optional;
-
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import com.anywhereworks.bookmarks.entities.Bookmark;
